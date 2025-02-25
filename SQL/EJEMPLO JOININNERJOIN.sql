f80cde0f602fc6e24f64d55e7b84edaf4819e05a
cprpython25 <cpellico79@gmail.com>
2025-02-25 20:29:05 +0100

    martes25

--- SQL/EJEMPLO JOININNERJOIN.sql
+++ SQL/EJEMPLO JOININNERJOIN.sql
@@ -1,11 +1,21 @@
 --Seleccionar el apellido, oficio, salario,
 --numero de departamento y su nombre de todos
 --los empleados cuyo salario sea mayor de 300000
-select EMP.APELLIDO, EMP.OFICIO, EMP.SALARIO, EMP.NUMERO,
-EMP.DEPT_NO
-from EMP
+select e.apellido
+, e.oficio
+, e.salario
+, d.dept_no as NUMERO
+, Dnombre as DEPARTAMENTO
+from EMP e
+inner join dept d on
+e.dept_no = d.dept_no
+where e.salario > 300000
 
-
+select s.Nombre as NOMBRESALA
+,h.Nombre as NOMBREHOSPITAL
+from sala s
+inner join hospital h on
+s.hospital_cod = h.hospital_cod
 
 
 
